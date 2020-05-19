a03b48514e10e2db11d44f135d19a86b4b45e7e8
Donatus Wolf <doni.wolf@googlemail.com>
2020-05-19 12:57:28 +0200

    create bar chart in background

--- code/S02-Data/index.js
+++ code/S02-Data/index.js
@@ -7,49 +7,58 @@ const app = new Vue({
     height: 300,
     step: 0,
     progress: 0,
-    margin: 64
+    margin: 64,
+    data: [] 
   },
-  mounted () {
+  mounted() {
     this.scroller.setup({
-      step: '.scrolly article .step',
-      offset: 0.5,
-      progress: true,
-      debug: false
-    }).onStepEnter(this.onEnter)
+        step: '.scrolly article .step',
+        offset: 0.5,
+        progress: true,
+        debug: false
+      }).onStepEnter(this.onEnter)
       .onStepProgress(this.onProgress)
       .onStepExit(this.onExit)
+
+      d3.csv('../assets/data/avocado.csv').then(data => {
+        this.data = data.map(d => {
+          return {
+            city: d.city,
+            price: +d.price,
+            volume: +d.volume
+          }
+        })
+        console.log(this.data);
+      })
 
     this.resize()
     window.addEventListener('resize', this.resize)
   },
   computed: {
-    innerHeight () {
-      return this.height - this.margin * 2
-    },
-    innerWidth () {
+    innerWidth() {
       return this.width - this.margin * 2
     },
-    barWidth () {
-      return this.innerWidth / this.data.length
+    innerHeight() {
+      return this.height - this.margin * 2
     }
   },
   methods: {
-    resize () {
+    resize() {
       const bounds = this.$refs.figure.getBoundingClientRect()
       this.width = bounds.width
       this.height = bounds.height
       this.scroller.resize()
     },
-    onEnter (step) {
+    onEnter(step) {
       console.log('enter', step)
       this.step = step.index
     },
-    onProgress (step) {
+    onProgress(step) {
       console.log('progress', step)
       this.step = step.index
       this.progress = step.progress
     },
-    onExit (step) {
+    onExit(step) {
       console.log('exit', step)
       this.step = step.index
     }
